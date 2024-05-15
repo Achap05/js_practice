32f7e087f0bae6aa5ea92d57553f3b8b9632a4c4
Achap05 <chapon.art@gmail.com>
2024-05-15 12:18:26 +0200

    add comparisons.js

---
+++ comparisons.js
@@ -0,0 +1,9 @@
+//Quel sera le résultat pour les expressions suivantes :
+
+5 > 4 // true  53>52 dans la table ASCII
+"apple" > "pineapple" // false 97<112 
+"2" > "12" // true 
+undefined == null  // true undefined n'est égal qu'à null sauf en égalité stricte 
+undefined === null // false même règle
+null == "\n0\n"    // false
+null === +"\n0\n"  //false
