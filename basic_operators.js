beb809a5837b8ea1280322f9511c903d8fd5384c
Achap05 <chapon.art@gmail.com>
2024-05-15 11:05:25 +0200

    add basic_operators.js

---
+++ basic_operators.js
@@ -0,0 +1,13 @@
+'use strict'
+//Quelles sont les valeurs finales de toutes les variables a, b, c et d après le code ci-dessous ?
+
+let a = 1, b = 1;
+
+let c = ++a; // 2 préfixe prends la nouvelle valeur
+let d = b++; // 1 postfixe prends l'ancienne valeur
+/* 
+Cependant a et b ont tout les deux été incrémenté donc a et b =2
+a=2
+b=2
+c=2
+d=1
